2117d53868487df33e99f28b8951c5fce0cca4e8
cokeIII <coke2484@gmail.com>
2023-02-27 23:24:49 +0700

    one

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "headerSetup.php" ?>
+</head>
+
+<body>
+    <div class="row bg-head text-light p-2">
+        <div class="col-md-12">
+            <div class="haed-time">
+                <h4>08.00 น. 25 กุมภาพันธ์ พ.ศ. 2565</h4>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div class="head-name-meetroom">
+                Meeting Room 1
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div class="head-status">
+                ว่าง
+            </div>
+        </div>
+    </div>
+    <div class="container body-row">
+        <div class="row justify-content-md-center mt-5">
+            <div class="col-md-4">
+                <div class="body-head bg-head text-light p-2 rounded-pill">
+                    <h5>รายการที่กำลังจะมาถึง</h5>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-md-center">
+            <div class="col-md-4">
+                <div class="text-light text-meet-detail mt-5">
+                    ประชุมครูผู้ช่วย
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-md-center">
+            <div class="col-md-4">
+                <div class="detail-time">
+                    09:00 - 10:45 น.
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-md-center">
+            <div class="col-md-4">
+                <div class="detail-by text-light">
+                    By ผอ.
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-md-center mt-5">
+        <div class="col-md-12  text-light">
+            <hr class="hr-footer">
+            <div class="meet-soon text-center">
+                Next ประชุมนักศึกษาฝึกงาน 13.30 - 15.30 น.
+            </div>
+            <hr class="hr-footer">
+        </div>
+    </div>
+</body>
+
+</html>
+<?php include "footerSetup.php" ?>
+<script>
+    $(document).ready(function() {})
+</script>
