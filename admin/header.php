21550b6dd5b9beefade47396a435a4754a02a860
cokeIII <coke2484@gmail.com>
2023-05-29 12:08:26 +0700

    fix name title bar

--- admin/header.php
+++ admin/header.php
@@ -4,7 +4,7 @@
 <meta name="description" content="">
 <meta name="author" content="">
 
-<title>CCT- Booking</title>
+<title>CTC- Booking</title>
 
 <!-- Custom fonts for this template-->
 <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
